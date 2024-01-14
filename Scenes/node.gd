c5f273a1113aafa26477e4af458aa3806a6ac983
unknown <workmail2159@gmail.com>
2024-01-14 13:58:38 -0700

    Added collision logic and Collider(ShapeCast2d) object

--- Scenes/node.gd
+++ Scenes/node.gd
@@ -1,7 +1,6 @@
 extends Node
 
 const mycelia_node = preload("res://Scenes/mycelia_node.tscn")
-@export var mycelia_nodes:Array[PackedScene]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,24 +10,37 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	#print($Collider.is_colliding)
 	pass
 	
 func _input(event):
-	# Mouse in viewport coordinates.
+	
 	if event is InputEventMouseButton and not event.is_action_released("click"):
-		print("Mouse Click/Unclick at: ", event.position)
-		add_mycelia_node(event.position)
+		# This runs if the mouse is clicked 
+		print("Mouse Click/Unclick at: ", )
+		if not check_collision(event.position):
+			# Checks collision at the mouse posiiton
+			print("no collision")
+			add_mycelia_node(event.position)
+		else:
+			print("collision")
 		
 	elif event is InputEventMouseMotion:
 		#print("Mouse Motion at: ", event.position)
 		pass
 
-   # Print the size of the viewport.
-	#print("Viewport Resolution is: ", get_viewport().get_visible_rect().size)
+
 
 func add_mycelia_node(pos):
+	# This function adds a mycelia node at a given position (pos)
 	var node = mycelia_node.instantiate()
-
 	add_child(node)
 	node.position = pos
-	
+
+func check_collision(pos):
+	#This function uses the Collider node to detect collisions at a given position pos
+	$Collider.position = pos
+	# Normally collider updates at the physics update step. This forces it to update
+	# prior to collision check. 
+	$Collider.force_shapecast_update() 
+	return $Collider.is_colliding()
