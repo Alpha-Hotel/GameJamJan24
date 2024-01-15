f54856e93f6f584dca462aba71a2ffe0aaf52d8d
unknown <workmail2159@gmail.com>
2024-01-15 15:25:15 -0700

    added expanding collision logic

--- Scenes/node.gd
+++ Scenes/node.gd
@@ -5,6 +5,10 @@ const mycelia_node = preload("res://Scenes/mycelia_node.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$HUD/Counter_number.text = "5"
+	var node = mycelia_node.instantiate()
+	add_child(node)
+	node.position = Vector2(400,400)
+	node.add_to_group("resource_nodes")
 	pass # Replace with function body.
 
 
@@ -26,13 +30,15 @@ func _input(event):
 	if event is InputEventMouseButton and not event.is_action_released("click"):
 		# This runs if the mouse is clicked 
 		print("Mouse Click/Unclick at: ", )
-		if not check_collision(event.position) and int($HUD/Counter_number.text)>0:
+		if not check_node_collision(event.position) and int($HUD/Counter_number.text)>0:
 			# Checks collision at the mouse posiiton
 			print("no collision")
 			add_mycelia_node(event.position)
 			$HUD/Counter_number.text = str(int($HUD/Counter_number.text)-1)
+			expanding_collision() #do something with this
 		else:
-			print("collision")
+			if int($HUD/Counter_number.text) > 0:
+				print("collision ", $Collider.scale)
 		##print("Mouse Click/Unclick at: ", event.position)
 
 		add_connecting_lines(event.position, closest_node_1, closest_node_2)
@@ -47,21 +53,40 @@ func add_mycelia_node(pos):
 	# This function adds a mycelia node at a given position (pos)
 	var node = mycelia_node.instantiate()
 	add_child(node)
-	print(node)
-	
 	node.position = pos
 	node.add_to_group("mycelia_nodes")
 	#print(get_tree().get_nodes_in_group("mycelia_nodes"))
 
 
-func check_collision(pos):
+func check_node_collision(pos):
 	#This function uses the Collider node to detect collisions at a given position pos
 	$Collider.position = pos
 	# Normally collider updates at the physics update step. This forces it to update
 	# prior to collision check. 
 	$Collider.force_shapecast_update() 
 	return $Collider.is_colliding()
+
+func expanding_collision():	
+	$Collider.scale =Vector2(20,20)
+	$Collider.force_shapecast_update() 
+	print("expanding collision ", $Collider.is_colliding())
+	$Collider.scale =Vector2(1,1)
 	
+	return get_non_player($Collider.collision_result)
+	
+
+func get_non_player(list):
+	var new_list = []
+	for i in list:
+		if not i["collider"] in get_tree().get_nodes_in_group("mycelia_nodes"):
+			new_list.append(i)
+			print("non-mycelia hit")
+		else:
+			print("mycelia node hit")
+		
+	return new_list
+
+
 func add_connecting_lines(placed_pos, pos_2, pos_3):
 	pass
 	
